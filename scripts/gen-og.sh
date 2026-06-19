c716ccbbaad0d3ca37c90fcdf5d56f645dd33dd3
Matteo Fortini <matteo.fortini@gmail.com>
2026-06-19 19:50:14 +0200

    Aggiungi gen-og.sh (Docker) e .gitignore
    - scripts/gen-og.sh: genera i PNG OG via Docker senza installare Node
    - .gitignore: esclude node_modules/

---
+++ scripts/gen-og.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Genera og-create.png e og-discover.png usando Docker (non serve Node locale).
+# Uso: ./scripts/gen-og.sh
+set -e
+cd "$(dirname "$0")/.."
+docker run --rm -v "$(pwd):/app" -w /app node:20-alpine sh -c "npm install --silent && npm run og"
